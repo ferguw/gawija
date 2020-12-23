a2ea167b51b7c4aab8cd4510e4fd830a10231a7f
ferguw <ferrgun17@gmail.com>
2020-12-23 16:00:52 +0800

    talent ljob action

--- talent/_content/job-tawaran-view.php
+++ talent/_content/job-tawaran-view.php
@@ -1,9 +1,33 @@
+<?php
+$idj = $_GET["id-job"];
+
+//Mencari Job pada tabel job
+$data_job_search = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM job WHERE `idj` = '$idj' "));
+
+//mengambil data dri tabel job
+$id_client = $data_job_search['idc'];
+
+//Mencari data client
+$data_client_search = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM client WHERE `idc` = '$id_client' "));
+
+//Mncari data requirement job (Type)
+$query_job_req = mysqli_query($con, "SELECT * FROM job_req WHERE `idj` = '$idj' ");
+//Mncari data requirement job (Value Job {jumlah talent, salary})
+$query_job_req_value = mysqli_query($con, "SELECT * FROM job_req WHERE `idj` = '$idj' ");
+
+
+if (isset($_POST['terima-pengajuan'])) {
+    mysqli_query($con, "UPDATE `tawaran` SET `status` = 'accept' WHERE `idj` = '$idj' AND `idt` = '$idt' ;");
+    echo "<script>window.location.href='?p=job-tawaran'</script>";
+}
+
+?>
 <!-- div::content -->
 <div class="row d-content">
     <!-- Begin::Card -->
     <div class="col-lg-12 page-content">
-        <h2 class="text-center">JUDUL JOB</h2>
-        <h4 class="text-center mb-5">comp</h4>
+        <h2 class="text-center"><?= ucwords($data_job_search['judul']); ?></h2>
+        <h4 class="text-center mb-5"><?= strtoupper($data_job_search['comp']); ?></h4>
 
         <!-- Card-Begin::Content -->
         <div class="row">
@@ -11,32 +35,42 @@
                 <div class="row align-items-center">
                     <div class="col-3 col-lg-1">
                         <div class="img-profil">
-                            <a href="#!" onclick="sidebar()"><img src="../assets/images/avatar/avatar-1.png"
-                                    alt="avatar-user" width="100%"></a>
+                            <a href="#!" onclick="sidebar()"><img src="../assets/images/avatar/avatar-1.png" alt="avatar-user" width="100%"></a>
                         </div>
                     </div>
                     <div class="col-9 col-lg-11">
-                        <h4>Nama Client</h4>
+                        <h4><?= ucwords($data_client_search['name']); ?></h4>
                     </div>
                     <div class="col-12 col-lg-11 offset-lg-1 detail-job mt-4">
                         <ul>
-                            <li><i class="material-icons">person_pin</i> &nbsp;Type, 1, 2, 3</li>
-                            <li><i class="material-icons">people</i> &nbsp;Required SPG : 3</li>
-                            <li><i class="material-icons">attach_money</i>&nbsp;Salary SPG : Rp <?=number_format('150000',0,",",'.')?> </li>
-                            <li><i class="material-icons">people</i> &nbsp;Required SPB : 3</li>
-                            <li><i class="material-icons">attach_money</i>&nbsp;Salary SPB : Rp <?=number_format('150000',0,",",'.')?> </li>
-                            <li><i class="material-icons">today</i>&nbsp;<span>2 Day</span></li>
-                            <li><i class="material-icons">location_on</i>&nbsp;<span>Location(city)</span></li>
-                            <li><i class="material-icons">location_city</i>&nbsp;<span>(Alamat) </span></li>
+                            <li><i class="material-icons">person_pin</i> &nbsp;Type
+                                <?php while ($data_job_req = mysqli_fetch_assoc($query_job_req)) {
+                                    echo ", " . $data_job_req['type'];
+                                } ?>
+                            </li>
+                            <?php
+                            while ($data_job_req_value = mysqli_fetch_assoc($query_job_req_value)) {
+                            ?>
+                                <li><i class="material-icons">people</i> &nbsp;Required <?= $data_job_req_value['type']; ?> : <?= $data_job_req_value['numtalent']; ?></li>
+                                <li><i class="material-icons">attach_money</i>&nbsp;Salary <?= $data_job_req_value['type']; ?> : Rp <?= number_format($data_job_req_value['salary'], 0, ",", '.') ?> </li>
+
+
+                            <?php } ?>
+
+                            <li><i class="material-icons">today</i>&nbsp;<span><?= $data_job_search['workday']; ?> Day</span></li>
+                            <li><i class="material-icons">location_on</i>&nbsp;<span><?= $data_job_search['city']; ?></span></li>
+                            <li><i class="material-icons">location_city</i>&nbsp;<span><?= $data_job_search['address']; ?> </span></li>
                         </ul>
-                           
+
                     </div>
-                    <div class="col-12 col-lg-11 offset-1"><hr></div>
+                    <div class="col-12 col-lg-11 offset-1">
+                        <hr>
+                    </div>
                 </div>
                 <div class="row">
                     <div class="col-12 col-lg-11 offset-lg-1">
                         <p>
-                            (Deksripsi) Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis ut dolorem deleniti porro rem praesentium illum? Laboriosam placeat, repudiandae tenetur consectetur repellat suscipit sequi amet illo vel, fuga beatae iusto!
+                            <?= ucwords($data_job_search['deskripsi']); ?>
                         </p>
                         <hr>
                     </div>
@@ -55,7 +89,17 @@
                         </div>
                     </div>
                     <div class="col-lg-4 offset-lg-1 text-center mt-3">
-                        <button type="submit" name="terima-pengajuan" class="btn btn-b1">Terima Permintaan</button>
+                        <form method="POST">
+                            <!-- <button type="submit" name="terima-pengajuan" class="btn btn-b1">Terima Permintaan</button> -->
+                            <?php
+                            $cari_job_ajuan_talent = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM tawaran WHERE `idj` = '$idj' AND `idt` = '$idt' "));
+                            if ($cari_job_ajuan_talent['status'] === 'pending' ) {
+                                echo '<button type="submit" name="terima-pengajuan" class="btn btn-b1">Terima Permintaan </button>';
+                            } else {
+                                echo '<button class="btn btn-b1">Anda Menerima Permintaan Ini </button>';
+                            }
+                            ?>
+                        </form>
                     </div>
                 </div>
 
